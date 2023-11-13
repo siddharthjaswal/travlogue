4c617de000fadad3280aae0a63ec145c27a7ff71
Sid <tosid23@gmail.com>
2023-11-13 15:40:38 +0530

    test push

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -47,7 +47,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material3.icons)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
