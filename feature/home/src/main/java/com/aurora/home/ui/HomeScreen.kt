54889fe6fdb67f5b8996cbdf6e95146a3ebeb32f
Sid <tosid23@gmail.com>
2023-11-17 16:31:39 +0530

    Added EmptyState

--- feature/home/src/main/java/com/aurora/home/ui/HomeScreen.kt
+++ feature/home/src/main/java/com/aurora/home/ui/HomeScreen.kt
@@ -1,7 +1,11 @@
 package com.aurora.home.ui
 
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -9,11 +13,13 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.aurora.home.R
 
 @Composable
 fun HomeScreen(
@@ -29,15 +35,28 @@ fun HomeScreen(
         },
         containerColor = MaterialTheme.colorScheme.background
     ) { innerPadding ->
+        EmptyState()
         Column(
             modifier = Modifier
                 .padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            Text(
-                text = "Home"
-            )
+
         }
+    }
+}
+
+@Composable
+internal fun EmptyState() {
+    Box(
+        Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.empty_state),
+            contentDescription = "Empty State",
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
 
@@ -48,7 +67,8 @@ internal fun Fab(onClickCreate: () -> Unit = {}) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode", showBackground = true)
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 internal fun Preview() {
     HomeScreen()
