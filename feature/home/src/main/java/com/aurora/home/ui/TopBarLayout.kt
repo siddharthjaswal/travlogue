23877785065f64cdf756563cf1f281426912a948
Sid <tosid23@gmail.com>
2025-03-06 01:39:00 +0530

    feat: Updated dependencies, removed main.md, and added Doto font family
    - Removed `main.md` file.
    - Updated `activity-compose` to version 1.10.1.
    - Updated `navigation-fragment-ktx` and `navigation-ui-ktx` to version 2.8.8.
    - Added Doto font family, including multiple variants (Black, Bold, ExtraBold, ExtraLight, Light, Medium, Regular, Rounded Black, Rounded Bold, Rounded ExtraBold, Rounded ExtraLight, Rounded Light, Rounded Medium, Rounded Regular, Rounded Semibold, Semibold, Thin).
    - Updated the `TopBarLayout` to use the Doto font for "Logue".
    - Updated inspection profiles.

--- feature/home/src/main/java/com/aurora/home/ui/TopBarLayout.kt
+++ feature/home/src/main/java/com/aurora/home/ui/TopBarLayout.kt
@@ -1,26 +1,40 @@
 package com.aurora.home.ui
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.aurora.designsystem.theme.AppTheme
+import com.aurora.designsystem.theme.dotoFamily
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun TopBarLayout() {
+
     TopAppBar(
         title = {
-            Text(
-                text = "Hello Traveller",
-                fontSize = 20.sp,
-                color = MaterialTheme.colorScheme.primary
-            )
+            Row {
+                Text(
+                    text = "Trav ",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Text(
+                    text = "Logue",
+                    fontFamily = dotoFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
         },
         actions = {
 
