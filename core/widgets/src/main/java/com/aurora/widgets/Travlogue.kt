bef4a37188fc762be8f0a280821954ff134aa073
Sid <tosid23@gmail.com>
2025-03-11 10:16:17 +0100

    feat: Added Travlogue icon and updated Travlogue widget
    - Added a new `ic_travlogue` icon to the `core:designsystem` module.
    - Updated `Travlogue` composable in `core:widgets` to include horizontal spacing between the icon and text.
    - Added `ic_travlogue` icon.

--- core/widgets/src/main/java/com/aurora/widgets/Travlogue.kt
+++ core/widgets/src/main/java/com/aurora/widgets/Travlogue.kt
@@ -1,5 +1,6 @@
 package com.aurora.widgets
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -7,13 +8,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aurora.designsystem.theme.pacificoFamily
 
 @Preview
 @Composable
 fun Travlogue() {
-    Row(modifier = Modifier, verticalAlignment = Alignment.CenterVertically) {
+    Row(
+        modifier = Modifier, verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
         Text(
             text = "Travlogue",
             fontFamily = pacificoFamily,
