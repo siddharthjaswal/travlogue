b75c5f626a842f765767158d28e2c98f14326735
Sid <tosid23@gmail.com>
2025-05-11 12:45:48 +0530

    refactor: Replaced `TextField` with `OutlinedTextField` in `BottomBar`
    - Replaced the standard `TextField` composable with `OutlinedTextField` in the `BottomBar` component.
    - Applied a `RoundedCornerShape` with 50% corner radius to the `OutlinedTextField`.
    - Added a preview for the `BottomBar` composable.

--- feature/home/src/main/java/com/aurora/home/ui/components/BottomBar.kt
+++ feature/home/src/main/java/com/aurora/home/ui/components/BottomBar.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,14 +25,21 @@ fun BottomBar() {
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        TextField(
+        OutlinedTextField(
             value = "",
             onValueChange = { /* Update state */ },
             placeholder = { Text("Where do you want to travel :) ? ", color = Color.Gray) },
             modifier = Modifier
                 .weight(1f)
                 .padding(horizontal = 8.dp),
-            singleLine = true
+            singleLine = true,
+            shape = RoundedCornerShape(percent = 50)
         )
     }
+}
+
+@Preview
+@Composable
+private fun BottomBarPreview() {
+    BottomBar()
 }
