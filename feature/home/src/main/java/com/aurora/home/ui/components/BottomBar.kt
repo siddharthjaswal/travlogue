6e1e2809e105e050792dafb66b0a0ace948dc762
Sid <tosid23@gmail.com>
2025-05-12 22:47:25 +0530

    refactor: Updated BottomBar TextField placeholder and added state management
    - Updated the placeholder text in the `OutlinedTextField` to "Help me plan: e.g., 'Paris for a week ?" from "Where do you want to travel :) ?".
    - Added `mutableStateOf` to manage the text input state for the `OutlinedTextField`.

--- feature/home/src/main/java/com/aurora/home/ui/components/BottomBar.kt
+++ feature/home/src/main/java/com/aurora/home/ui/components/BottomBar.kt
@@ -9,6 +9,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,6 +22,8 @@ import androidx.compose.ui.unit.dp
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomBar() {
+    var textInput by remember { mutableStateOf("") }
+
     Row(
         modifier = Modifier
             .padding(horizontal = 8.dp, vertical = 12.dp)
@@ -26,9 +32,9 @@ fun BottomBar() {
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         OutlinedTextField(
-            value = "",
-            onValueChange = { /* Update state */ },
-            placeholder = { Text("Where do you want to travel :) ? ", color = Color.Gray) },
+            value = textInput,
+            onValueChange = { textInput = it },
+            placeholder = { Text("Help me plan: e.g., 'Paris for a week ? ", color = Color.Gray) },
             modifier = Modifier
                 .weight(1f)
                 .padding(horizontal = 8.dp),
