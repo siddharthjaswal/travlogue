a10e5ee825afddef96834e59d2261ab1555caba0
Sid <tosid23@gmail.com>
2025-06-14 12:43:41 +0530

    feat: Display trip duration in timeline
    - Added a `Text` composable to `GetTimelineLayout.kt` to display the number of days in a trip.
    - The text is centered and styled with a font size of 18.sp.

--- feature/genesis/src/main/java/com/aurora/genesis/ui/GetTimelineLayout.kt
+++ feature/genesis/src/main/java/com/aurora/genesis/ui/GetTimelineLayout.kt
@@ -3,11 +3,16 @@ package com.aurora.genesis.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.aurora.data.data.entity.trip.TripEntity
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -15,13 +20,18 @@ import com.aurora.data.data.entity.trip.TripEntity
 internal fun GetTimelineLayout(
     trip: TripEntity
 ) {
-
     Column(
         modifier = Modifier
             .padding(16.dp)
             .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = "${trip.days} Days",
+            fontWeight = FontWeight.Normal,
+            fontSize = 18.sp
+        )
     }
 }
